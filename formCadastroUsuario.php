fa210cf12de5dad19567c7bd54a50f43c5106416
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-05-02 21:18:19 -0300

    Add files via upload

--- formCadastroUsuario.php
+++ formCadastroUsuario.php
@@ -18,12 +18,35 @@
       	</div>	
       	<div class="row">
 				<div class="col-sm-6">
-      			
+      			<form action="/action_page.php">
+					  <div class="form-group">
+					    <label for="email">Email address:</label>
+					    <input type="email" class="form-control" id="email">
+					  </div>
       		</div>
       		<div class="col-sm-6">
-      			
+      			<div class="form-group">
+					    <label for="pwd">Password:</label>
+					    <input type="password" class="form-control" id="pwd">
+					  </div>
       		</div>
-      	</div>					     	
+      	</div>	
+      	<div class="row">
+				<div class="col-sm-4	">
+					  <div class="form-group">
+					    <label for="email">Email address:</label>
+					    <input type="email" class="form-control" id="email">
+					  </div>
+      		</div>
+      		<div class="col-sm-6">
+      			<div class="form-group">
+					    <label for="pwd">Password:</label>
+					    <input type="password" class="form-control" id="pwd">
+					  </div>
+      		</div>
+      	</div>	
+      	 <button type="submit" class="btn btn-primary">Submit</button>
+      	  <button type="submit" class="btn btn-success">Submit</button>			     	
     </div>
   </div>
 </div>
